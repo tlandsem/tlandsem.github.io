3a70103eb21feaabf9d194fd5eabdb7a1faad627
tlandsem <trinelandsem@gmail.com>
2026-02-11 17:13:20 +0100

    Add files via upload

---
+++ lightbox.js
@@ -0,0 +1,61 @@
+const images = document.querySelectorAll(".galleri img");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeBtn = document.querySelector(".close");
+const nextBtn = document.querySelector(".right");
+const prevBtn = document.querySelector(".left");
+const footer = document.getElementById("footer");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  currentIndex = index;
+  lightboxImg.src = images[currentIndex].src;
+  lightbox.style.display = "flex";
+  footer.style.display = "none";
+}
+
+images.forEach((img, index) => {
+  img.addEventListener("click", () => showImage(index));
+});
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex =
+    (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
+
+nextBtn.addEventListener("click", nextImage);
+prevBtn.addEventListener("click", prevImage);
+
+closeBtn.addEventListener("click", () => {
+  lightbox.style.display = "none";
+  footer.style.display = "block";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+    footer.style.display = "block";
+  }
+});
+
+document.addEventListener("keydown", (e) => {
+  if (lightbox.style.display === "flex") {
+    if (e.key === "ArrowRight") nextImage();
+    if (e.key === "ArrowLeft") prevImage();
+    if (e.key === "Escape") {
+        lightbox.style.display = "none";
+        footer.style.display = "block";
+   }
+  }
+});
+
+document.querySelectorAll("img").forEach(img => {
+  img.addEventListener("contextmenu", e => e.preventDefault());
+});
